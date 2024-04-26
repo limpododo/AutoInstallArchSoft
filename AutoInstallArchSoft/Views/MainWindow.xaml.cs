f60da0e8b47bef72f06dad2419ba69e8ace9b9c2
Max Duh <duhanin@npfratex.ru>
2024-04-26 19:10:08 +0300

    Перенес в папку mainview

---
+++ AutoInstallArchSoft/Views/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+
+namespace AutoInstallArchSoft;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Close(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}
